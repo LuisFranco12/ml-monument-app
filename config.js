2f896871aeec294a12d9ecf14118f4b8528cfba7
LuisFranco12 <108766758+LuisFranco12@users.noreply.github.com>
2026-04-06 18:19:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,44 @@
+// window.ML_APP_CONFIG = {
+//   cloudinary: {
+//     enabled: true,
+//     cloudName: 'dcrusohw7',
+//     uploadPreset: 'monument_photos'
+//   },
+//   airtable: {
+//     enabled: true,
+//     baseId: 'appDEGBJxfr14dLbn',
+//     tableName: 'Saved Jobs',
+//     personalAccessToken: 'patBx4YWW6wATtuD3.80263f849ea7dfe89f8ebf484697ac590fbc7271159987980fcdcf84b544f83a',
+//     fields: {
+//       ownerName: 'Owner Name',
+//       ownerId: 'Owner ID',
+//       plot: 'Plot',
+//       jobName: 'Job Name',
+//       date: 'Date',
+//       jobType: 'Job Type',
+//       areaSqFt: 'Area Sq Ft',
+//       cleanML: 'Clean ML',
+//       cleanTotal: 'Clean Total',
+//       exteriorML: 'Exterior ML',
+//       exteriorTotal: 'Exterior Total',
+//       repointingLF: 'Re-pointing LF',
+//       repointingML: 'Re-pointing ML',
+//       repointingTotal: 'Re-pointing Total',
+//       flats: 'Flats',
+//       slants: 'Slants',
+//       jobTotal: 'Job Total',
+//       notes: 'Notes',
+//       detailsJson: 'Details JSON',
+//       photoCount: 'Photo Count',
+//       photos: 'Photos',
+//       photoUrls: 'Photo URLs',
+//       issueType: 'Issue Type',
+//       issueNote: 'Issue Note'
+//     }
+//   }
+// };
+
 window.ML_APP_CONFIG = {
-  cloudinary: {
-    enabled: true,
-    cloudName: 'dcrusohw7',
-    uploadPreset: 'monument_photos'
-  },
   airtable: {
     enabled: true,
     baseId: 'appDEGBJxfr14dLbn',
@@ -13,6 +48,10 @@ window.ML_APP_CONFIG = {
       ownerName: 'Owner Name',
       ownerId: 'Owner ID',
       plot: 'Plot',
+      photos: 'Photos',
+      photoUrls: 'Photo URLs',
+      issueType: 'Issue Type',
+      issueNote: 'Issue Note',
       jobName: 'Job Name',
       date: 'Date',
       jobType: 'Job Type',
@@ -29,11 +68,11 @@ window.ML_APP_CONFIG = {
       jobTotal: 'Job Total',
       notes: 'Notes',
       detailsJson: 'Details JSON',
-      photoCount: 'Photo Count',
-      photos: 'Photos',
-      photoUrls: 'Photo URLs',
-      issueType: 'Issue Type',
-      issueNote: 'Issue Note'
+      photoCount: 'Photo Count'
     }
+  },
+  cloudinary: {
+    cloudName: 'dcrusohw7',
+    uploadPreset: 'monument_photos'
   }
 };
